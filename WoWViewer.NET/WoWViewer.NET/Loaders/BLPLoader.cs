3a9f59c7529966f63ef32d51c74eaf942377b7b3
Martin Benjamins <ictmartin@gmail.com>
2024-01-01 16:49:02 +0100

    Direct texture uploads for DXT textures

--- WoWViewer.NET/WoWViewer.NET/Loaders/BLPLoader.cs
+++ WoWViewer.NET/WoWViewer.NET/Loaders/BLPLoader.cs
@@ -24,23 +24,53 @@ namespace WoWViewer.NET.Loaders
             using (var blp = new BlpFile(CASC.OpenFile(fileDataID)))
             {
                 Console.WriteLine("Loading BLP " + fileDataID + " " + blp.preferredFormat.ToString());
-
                 gl.BindTexture(TextureTarget.Texture2D, textureID);
 
-                //for (var i = 0; i < 1; i++)
-                //{
-                    var bmp = blp.GetImage(0);
-                    if (bmp == null)
-                        throw new Exception("BMP is null!");
+                if (
+                    blp.preferredFormat == BlpPixelFormat.Dxt1 ||
+                    blp.preferredFormat == BlpPixelFormat.Dxt3 ||
+                    blp.preferredFormat == BlpPixelFormat.Dxt5
+                )
+                {
+                    var openGLFormat = InternalFormat.CompressedRgbaS3TCDxt5Ext;
 
+                    if (blp.preferredFormat == BlpPixelFormat.Dxt1 && blp.alphaSize > 0)
+                        openGLFormat = InternalFormat.CompressedRgbaS3TCDxt1Ext;
+                    else if (blp.preferredFormat == BlpPixelFormat.Dxt1 && blp.alphaSize == 0)
+                        openGLFormat = InternalFormat.CompressedRgbS3TCDxt1Ext;
+                    else if (blp.preferredFormat == BlpPixelFormat.Dxt3)
+                        openGLFormat = InternalFormat.CompressedRgbaS3TCDxt3Ext;
+
+                    var maxMip = 0;
+
+                    for(int i = 0; i < blp.MipMapCount; i++)
+                    {
+                        var bytes = blp.GetPixels(i, out int width, out int height, true, true);
+
+                        if (width == 0 || height == 0 || bytes.Length == 0)
+                            continue;
+
+                        maxMip = i;
+
+                        fixed (byte* buf = bytes)
+                            gl.CompressedTexImage2D(TextureTarget.Texture2D, i, openGLFormat, (uint)width, (uint)height, 0, (uint)bytes.Length, buf);
+                    }
+
+                    gl.TexParameter(TextureTarget.Texture2D, TextureParameterName.TextureMaxLevel, maxMip);
+                    gl.TexParameter(TextureTarget.Texture2D, TextureParameterName.TextureMinFilter, (int)TextureMinFilter.LinearMipmapLinear);
+                }
+                else
+                {
+                    var bmp = blp.GetImage(0) ?? throw new Exception("BMP is null!");
                     var pixelBytes = new byte[bmp.Width * bmp.Height * 4];
                     bmp.CopyPixelDataTo(pixelBytes);
 
                     fixed (byte* buf = pixelBytes)
                         gl.TexImage2D(GLEnum.Texture2D, 0, InternalFormat.Rgba, (uint)bmp.Width, (uint)bmp.Height, 0, PixelFormat.Rgba, PixelType.UnsignedByte, buf);
-                //}
 
-                gl.TexParameter(TextureTarget.Texture2D, TextureParameterName.TextureMinFilter, (int)TextureMinFilter.Linear);
+                    gl.TexParameter(TextureTarget.Texture2D, TextureParameterName.TextureMinFilter, (int)TextureMinFilter.Linear);
+                }
+
                 gl.TexParameter(TextureTarget.Texture2D, TextureParameterName.TextureMagFilter, (int)TextureMagFilter.Linear);
                 gl.TexParameter(TextureTarget.Texture2D, TextureParameterName.TextureWrapS, (int)TextureWrapMode.Repeat);
                 gl.TexParameter(TextureTarget.Texture2D, TextureParameterName.TextureWrapT, (int)TextureWrapMode.Repeat);
