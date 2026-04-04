0c89dd7477e8a780306891644e7c48cf70d46af8
Martin Benjamins <ictmartin@gmail.com>
2026-04-04 16:39:01 +0200

    Store WMO/M2 in containers

--- WoWViewer.NET/WoWViewer.NET/Objects/M2Container.cs
+++ WoWViewer.NET/WoWViewer.NET/Objects/M2Container.cs
@@ -8,9 +8,12 @@ namespace WoWViewer.NET.Objects
         public bool[] EnabledGeosets { get; }
 
         public bool forceRender { get; set; } = false;
+
+        private Renderer.Structs.DoodadBatch m2;
+
         public M2Container(GL gl, uint fileDataID, uint shaderProgram, uint parentFileDataId) : base(gl, fileDataID, shaderProgram, parentFileDataId)
         {
-            var m2 = Cache.GetOrLoadM2(gl, fileDataID, shaderProgram, parentFileDataId);
+            m2 = Cache.GetOrLoadM2(gl, fileDataID, shaderProgram, parentFileDataId);
             EnabledGeosets = new bool[m2.submeshes.Length];
             Array.Fill(EnabledGeosets, true);
 
--- WoWViewer.NET/WoWViewer.NET/Objects/WMOContainer.cs
+++ WoWViewer.NET/WoWViewer.NET/Objects/WMOContainer.cs
@@ -9,10 +9,11 @@ namespace WoWViewer.NET.Objects
         public bool[] EnabledDoodadSets { get; }
 
         public uint UniqueID;
-
+        private Renderer.Structs.WorldModel wmo;
         public WMOContainer(GL gl, uint fileDataID, uint shaderProgram, uint parentFileDataId) : base(gl, fileDataID, shaderProgram, parentFileDataId)
         {
-            var wmo = Cache.GetOrLoadWMO(gl, fileDataID, shaderProgram, parentFileDataId);
+            wmo = Cache.GetOrLoadWMO(gl, fileDataID, shaderProgram, parentFileDataId);
+
             EnabledGroups = new bool[wmo.groupBatches.Length];
             Array.Fill(EnabledGroups, true);
             EnabledDoodadSets = new bool[wmo.doodadSets.Length];
